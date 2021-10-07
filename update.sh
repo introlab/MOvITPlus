5a0286fbdad80f8e6140d9482f514bc85bdacbd3
Dominic Létourneau <dominic.letourneau@usherbrooke.ca>
2021-10-07 09:45:36 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,6 +4,12 @@ sudo systemctl stop movit_backend.service
 sudo systemctl stop movit_frontend.service
 sudo systemctl stop movit_detect_python.service
 
+# Update service file
+sudo cp config/services/movit_backend.service /lib/systemd/system
+sudo cp config/services/movit_frontend.service /lib/systemd/system
+sudo cp config/services/movit_detect_python.service /lib/systemd/system
+sudo systemctl daemon-reload
+
 # Update sources
 echo "MAJ code"
 # Synchronize submodule URLs
